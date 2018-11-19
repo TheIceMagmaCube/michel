aecbd7736d3ff6d32a89832584219872155ca77b
TheIceMagmaCube <38074756+TheIceMagmaCube@users.noreply.github.com>
2018-11-19 17:10:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const cfg = require('./index.json');
+const prefix = ("//");
+
+bot.on('ready', function () {
+    console.log("Je suis prêt et opérationnel.")
+    bot.user.setActivity("regarder comment défoncer le serveur").catch(console.error)
+    
+})
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send("Bienvenue sur le serveur de test du Studio Digido, ce serveur est un test !\n\nVous restez à vos risques et périls " + member.displayName + " !")
+        console.log(`${member.displayName} à rejoint le serveur.`)
+    }).catch(console.error)
+
+})
+
+bot.login(cfg.token);
