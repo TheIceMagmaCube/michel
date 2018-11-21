366eb3924ac1d0ca98f4d94a5d25971ad131a5f0
TheIceMagmaCube <38074756+TheIceMagmaCube@users.noreply.github.com>
2018-11-21 13:37:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -263,4 +263,15 @@ client.on("message", async message => {
 		if(dispatcher !== undefined)
 			dispatcher.resume();
 	}
+	
+	if(commande === "join")
+		if (message.member.voiceChannel) {
+        	message.member.voiceChannel.join()
+          	.then(connection => { // Connection is an instance of VoiceConnection
+           	 message.reply('Je suis connecté dans votre channel !');
+          	})
+          		.catch(console.log);
+      			} else {
+        		message.reply('Les samouraïs de Discord se sont perdus en route !\n\n Essayez de changer de salon vocal');
+      }
 });
